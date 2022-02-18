97bf9ad6f279332f07799efb99b31a8a6287b58b
alexandre jamarco <alexandre.j@gmail.com>
2022-02-18 15:55:31 +0000

    fixed freeze dice functionality

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
+  constructor(props){
+    super(props);
+    this.handleRuleClick = this.handleRuleClick.bind(this);
+  }
+
+  handleRuleClick = e => {
+    console.log('inside handle rule click current score: ',this.props.score);
+    if(this.props.score !== undefined) return;
+    this.props.doScore(this.props.name,`${this.props.name}.evalRoll`);
+
+  }
+
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className={`RuleRow RuleRow-${!this.props.score ? 'active': 'disabled' }`} onClick={this.handleRuleClick} >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
